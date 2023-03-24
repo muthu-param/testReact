366e1ef6dbd62ffebd418b7447c2c1a68eeeba12
muthukumar <muthu@param.network>
2023-03-24 15:36:52 +0530

    deliver script added

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -19,6 +19,7 @@ echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
 # npm start &
+npm install -g serve
 serve build -s -p 5004
 sleep 1
 echo $! > .pidfile
